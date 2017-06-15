2c46d9745fd1a9e55c816943846bc9f70edcfe3f
Jurvis Tan <jurvis.tan@me.com>
2017-06-15 23:00:16 +0800

    add first application typings

---
+++ src/typings/city.d.ts
@@ -0,0 +1,7 @@
+interface City {
+  id: number;
+  city_name: string;
+  data: number;
+  temperature: number;
+  time_scraped: string;
+}
---
+++ src/typings/interfaces.d.ts
@@ -0,0 +1,4 @@
+interface IAppState {
+  cities: Array<City>;
+  nearbyCity?: City;
+}
